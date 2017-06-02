34a94a71f8fcd1189636696876770542ffa09809
yii2-developer <yii2.developer@gmail.com>
2017-06-02 18:47:55 +0500

    rules

--- html/framework/config/rules.php
+++ html/framework/config/rules.php
@@ -13,13 +13,4 @@ return [
     '<language:\w+\-\w+>/<module:[\w\-]+>/<controller:[\w\-]+>/<action:[\w\-]+>/<p:\d+>/<per:\d+>' => '<module>/<controller>/<action>',
     '<language:\w+\-\w+>/<module:[\w\-]+>/<controller:[\w\-]+>/<action:[\w\-]+>/<id:\d+>' => '<module>/<controller>/<action>',
     '<language:\w+\-\w+>/<module:[\w\-]+>/<controller:[\w\-]+>/<action:[\w\-]+>' => '<module>/<controller>/<action>',
-    /**
-     * Системные правила
-     */
-    /* cp */
-    '<language:\w+\-\w+>/cp' => 'cp',
-    '<language:\w+\-\w+>/cp/<module:[\w\-]+>' => 'cp/<module>',
-    '<language:\w+\-\w+>/cp/<module:[\w\-]+>/<controller:[\w\-]+>' => 'cp/<module>/<controller>',
-    // todo: id | p | per
-    '<language:\w+\-\w+>/cp/<module:[\w\-]+>/<controller:[\w\-]+>/<action:[\w\-]+>' => 'cp/<module>/<controller>/<action>',
 ];
