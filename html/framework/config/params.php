b1a36156f7f26362d194055d9d472ced95155ea1
yii2-developer <yii2.developer@gmail.com>
2017-06-07 13:18:35 +0500

    fix: params

--- html/framework/config/params.php
+++ html/framework/config/params.php
@@ -2,4 +2,53 @@
 
 return [
     'email' => 'webmaster@dev-vps.ru',
+    'HTMLPurifier' => [
+        'Attr.AllowedFrameTargets' => [
+            '_blank',
+            '_self',
+            '_parent',
+            '_top',
+        ],
+        'HTML.Trusted' => true,
+        'Filter.YouTube' => true,
+    ],
+    'menu' => [
+        [
+            'label' => 'Администрирование',
+            'icon' => 'ti-panel',
+            'items' => [
+                [
+                    'label' => 'Авторизация',
+                    'icon' => 'ti-user',
+                    'url' => ['/auth'],
+                ],
+                [
+                    'label' => 'Социальные сети',
+                    'icon' => 'ti-sharethis',
+                    'url' => ['/auth/social/index'],
+                ],
+                [
+                    'label' => 'Журнал',
+                    'icon' => 'ti-book',
+                    'url' => ['/auth/log/index'],
+                ],
+            ],
+        ],
+        [
+            'label' => 'Системные',
+            'icon' => 'ti-settings',
+            'items' => [
+                [
+                    'label' => 'Очистить кэш',
+                    'icon' => 'ti-reload',
+                    'url' => ['/system/default/flush-cache'],
+                ],
+                [
+                    'label' => 'Очистить ресурсы',
+                    'icon' => 'ti-trash',
+                    'url' => ['/system/default/flush-assets'],
+                ],
+            ],
+        ],
+    ],
 ];
