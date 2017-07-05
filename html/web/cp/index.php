94aa635345c64ce89723bc37cfe651f6bcf3eeab
yii2-developer <yii2.developer@gmail.com>
2017-07-05 11:34:23 +0500

    dotenv

--- html/web/cp/index.php
+++ html/web/cp/index.php
@@ -4,11 +4,10 @@ ini_set('session.use_strict_mode', true);
 ini_set('session.sid_length', 128);
 ini_set('session.sid_bits_per_character', 6);
 
-// comment out the following two lines when deployed to production
-defined('YII_DEBUG') or define('YII_DEBUG', false);
-defined('YII_ENV') or define('YII_ENV', 'prod');
-
 require(__DIR__ . '/../../framework/vendor/autoload.php');
+
+require(__DIR__ . '/../../framework/config/env.php');
+
 require(__DIR__ . '/../../framework/vendor/yiisoft/yii2/Yii.php');
 
 $config = require(__DIR__ . '/../../framework/config/backend.php');
