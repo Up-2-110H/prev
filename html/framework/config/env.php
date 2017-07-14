bc8174edefe090cae1226679ef8df78d917fda7f
yii2-developer <yii2.developer@gmail.com>
2017-07-14 15:22:53 +0500

    fix: .env

--- html/framework/config/env.php
+++ html/framework/config/env.php
@@ -3,5 +3,5 @@
 $dotEnv = new \Dotenv\Dotenv(dirname(dirname(dirname(__DIR__))));
 $dotEnv->load();
 
-defined('YII_DEBUG') or define('YII_DEBUG', getenv('YII_DEBUG'));
+defined('YII_DEBUG') or define('YII_DEBUG', getenv('YII_DEBUG') == 'true');
 defined('YII_ENV') or define('YII_ENV', getenv('YII_ENV'));
