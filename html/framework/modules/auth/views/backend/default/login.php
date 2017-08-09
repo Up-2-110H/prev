8c97b0910bdd8ea336e4b14d659cc9ad9d26d53d
yii2-developer <yii2.developer@gmail.com>
2017-08-09 15:18:51 +0500

    form login

--- html/framework/modules/auth/views/backend/default/login.php
+++ html/framework/modules/auth/views/backend/default/login.php
@@ -2,6 +2,7 @@
 
 use yii\captcha\Captcha;
 use yii\helpers\Html;
+use yii\helpers\Url;
 use yii\widgets\ActiveForm;
 
 /* @var $this yii\web\View */
@@ -12,39 +13,57 @@ $this->title = 'Авторизация';
 ?>
 <div class="container">
     <div class="row">
-        <div class="col-md-4 col-sm-6 col-md-offset-4 col-sm-offset-3">
+        <div class="col-md-6 col-sm-10 col-md-offset-3 col-sm-offset-1">
             <?php $form = ActiveForm::begin([
                 'fieldConfig' => [
                     'template' => '{label}' . PHP_EOL . '{input}' . PHP_EOL . '{error}',
                 ],
             ]); ?>
-            <div class="card" data-background="color" data-color="blue">
-                <div class="card-header">
-                    <h3 class="card-title">Пожалуйста авторизуйтесь</h3>
+            <div class="card card-login swal2-show" data-background="color" data-color="blue">
+                <div class="container-fluid">
+                    <div class="row">
+                        <div class="col-lg-12">
+                            <div class="card-login__title">
+                                <a href="<?= Url::to() ?>" class="logo-big">
+                                    <?= Html::img('@web/static/default/img/logo-big.svg', ['alt' => 'Лого']) ?>
+                                </a>
+                                <span class="name"><?= Yii::$app->name ?></span>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="card-login__body row">
+                        <div class="col-lg-6">
+                            <div class="form-group">
+                                <?= $form->field($model, 'login')->textInput(['autofocus' => 'autofocus']) ?>
+                            </div>
+                        </div>
+                        <div class="col-lg-6">
+                            <div class="form-group">
+                                <?= $form->field($model, 'password')->passwordInput() ?>
+                            </div>
+                        </div>
+                    </div>
                 </div>
-                <div class="card-content">
-                    <div class="form-group">
-                        <?= $form->field($model, 'login')->textInput(
-                            ['class' => 'form-control input-no-border']
-                        ) ?>
-                    </div>
-                    <div class="form-group">
-                        <?= $form->field($model, 'password')->passwordInput(
-                            ['class' => 'form-control input-no-border']
-                        ) ?>
-                    </div>
-                    <div class="form-group">
-                        <?= $form->field($model, 'verifyCode')->widget(
-                            Captcha::className(), [
-                            'captchaAction' => '/auth/default/captcha',
-                            'options' => [
-                                'class' => 'form-control',
-                            ],
-                        ]) ?>
+                <div class="container-fluid gray-bg">
+                    <div class="card-login__vertify row">
+                        <div class="col-lg-12">
+                            <div class="form-group">
+                                <?= $form->field($model, 'verifyCode')->widget(
+                                    Captcha::className(), [
+                                    'captchaAction' => '/auth/default/captcha',
+                                ]) ?>
+                            </div>
+                        </div>
                     </div>
                 </div>
-                <div class="card-footer text-center">
-                    <?= Html::submitButton('Авторизация', ['class' => 'btn btn-fill btn-wd']) ?>
+                <div class="container-fluid">
+                    <div class="card-login__footer row">
+                        <div class="col-lg-12">
+                            <div class="text-center">
+                                <?= Html::submitButton('Авторизация', ['class' => 'btn btn-fill btn-wd']) ?>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
             <?php ActiveForm::end(); ?>
