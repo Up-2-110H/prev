14ac8f1bb6c5245ac9d55bda136e545ab7e7e1c4
yii2-developer <yii2.developer@gmail.com>
2018-01-08 11:50:24 +0500

    fix: post-merge.sh

--- .git-templates/hooks/post-merge.sh
+++ .git-templates/hooks/post-merge.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env sh
 
-./docker.sh exec framework/yii migrate/up-all
+./docker.sh exec framework/yii migrate/up
 ./docker.sh exec framework/yii access/install
 ./docker.sh exec framework/yii cache/flush-all
