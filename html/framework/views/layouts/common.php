c3de4c2db78589ac3180fd98e680ad19a8d11769
yii2-developer <yii2.developer@gmail.com>
2018-04-20 11:40:25 +0500

    fix: yii-2.0.15.1

--- html/framework/views/layouts/common.php
+++ html/framework/views/layouts/common.php
@@ -1,14 +1,13 @@
 <?php
 
-/* @var $this \yii\web\View */
-
-/* @var $content string */
-
 use app\assets\AppAsset;
 use yii\bootstrap\Nav;
 use yii\bootstrap\NavBar;
 use yii\helpers\Html;
 use yii\widgets\Breadcrumbs;
+
+/* @var $this \yii\web\View */
+/* @var $content string */
 
 AppAsset::register($this);
 ?>
@@ -17,8 +16,9 @@ AppAsset::register($this);
 <html lang="<?= Yii::$app->language ?>">
 <head>
     <meta charset="<?= Yii::$app->charset ?>">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <?= Html::csrfMetaTags() ?>
+    <?php $this->registerCsrfMetaTags() ?>
     <title><?= Html::encode($this->title) ?></title>
     <?php $this->head() ?>
 </head>
