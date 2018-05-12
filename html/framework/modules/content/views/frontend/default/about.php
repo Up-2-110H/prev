9e7218ce77a1a335263450e86f80044f9dbd0102
yii2-developer <yii2.developer@gmail.com>
2018-05-12 12:25:20 +0500

    cs

--- html/framework/modules/content/views/frontend/default/about.php
+++ html/framework/modules/content/views/frontend/default/about.php
@@ -1,10 +1,9 @@
 <?php
 
-/* @var $this yii\web\View */
-
-/* @var $dto \krok\content\dto\frontend\ContentDto */
-
 use yii\helpers\Html;
+
+/* @var $this \yii\web\View */
+/* @var $dto \krok\content\dto\frontend\ContentDto */
 
 $this->title = $dto->getTitle();
 $this->params['breadcrumbs'][] = $this->title;
@@ -13,7 +12,10 @@ $this->registerMetaTag(['name' => 'keywords', 'content' => $dto->getKeywords()])
 $this->registerMetaTag(['name' => 'description', 'content' => $dto->getDescription()]);
 ?>
 <div class="site-about">
-    <h1><?= Html::encode($this->title) ?></h1>
+
+    <h1>
+        <?= Html::encode($this->title) ?>
+    </h1>
 
     <p>
         <?= $dto->getText() ?>
--- html/framework/modules/content/views/frontend/default/error.php
+++ html/framework/modules/content/views/frontend/default/error.php
@@ -1,18 +1,19 @@
 <?php
 
-/* @var $this yii\web\View */
+use yii\helpers\Html;
+
+/* @var $this \yii\web\View */
 /* @var $name string */
 /* @var $message string */
-
-/* @var $exception Exception */
-
-use yii\helpers\Html;
+/* @var $exception \Exception */
 
 $this->title = $name;
 ?>
 <div class="site-error">
 
-    <h1><?= Html::encode($this->title) ?></h1>
+    <h1>
+        <?= Html::encode($this->title) ?>
+    </h1>
 
     <div class="alert alert-danger">
         <?= nl2br(Html::encode($message)) ?>
@@ -21,6 +22,7 @@ $this->title = $name;
     <p>
         The above error occurred while the Web server was processing your request.
     </p>
+
     <p>
         Please contact us if you think this is a server error. Thank you.
     </p>
