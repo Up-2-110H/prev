4cddb0f449c3b2dbae6d437362ef546ac59782cb
yii2-developer <yii2.developer@gmail.com>
2018-08-20 15:41:38 +0500

    config: datetimeFormatter

--- html/framework/config/backend.php
+++ html/framework/config/backend.php
@@ -35,6 +35,9 @@ $config = [
             \krok\extend\grid\DatePickerColumn::class => [
                 'format' => 'datetime',
             ],
+            \krok\flatpickr\FlatpickrDatetimeWidget::class => \krok\datetimeFormatter\FlatpickrDatetimeWidget::class,
+            \krok\flatpickr\FlatpickrDateWidget::class => \krok\datetimeFormatter\FlatpickrDateWidget::class,
+            \krok\flatpickr\FlatpickrTimeWidget::class => \krok\datetimeFormatter\FlatpickrTimeWidget::class,
             \krok\editor\EditorWidget::class => \krok\tinymce\TinyMceWidget::class,
             \krok\tinymce\TinyMceWidget::class => [
                 'clientOptions' => [
